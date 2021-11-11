e2029453abc6be762ddd2c928d03a5aaa0e527bb
unknown <arian.angoma@tecsup.edu.pe>
2021-11-11 04:54:43 -0500

    Formulario de Login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,25 +1,61 @@
 import {Link} from 'react-router-dom';
 
+/* Importaciones propias */
+import {useForm} from '../hooks/useForm';
+
+/* Estado inicial del formulario de Login */
+const initialStateFormLogin = {
+    email: '',
+    password: '',
+    rememberMe: false
+}
+
 export const LoginPage = () => {
+    /* Hook para el formulario */
+    const [formValues, handleInputChange, reset] = useForm(initialStateFormLogin);
+    const {email, password, rememberMe} = formValues;
+
+    const toggleCheck = () => {
+        reset({
+            ...formValues,
+            rememberMe: !rememberMe
+        });
+    }
+
+    /* Función para el submit del login */
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        console.log(formValues);
+    }
+
     return (
-        <form className="login100-form validate-form flex-sb flex-w">
+        <form className="login100-form validate-form flex-sb flex-w"
+              onSubmit={handleLogin}>
             <span className="login100-form-title mb-3">
                 Chat - Ingreso
             </span>
 
             <div className="wrap-input100 validate-input mb-3">
-                <input className="input100" type="email" name="email" placeholder="Email"/>
+                <input className="input100" type="email" name="email" placeholder="Email"
+                       value={email}
+                       onChange={handleInputChange}/>
                 <span className="focus-input100"/>
             </div>
 
             <div className="wrap-input100 validate-input mb-3">
-                <input className="input100" type="password" name="password" placeholder="Password"/>
+                <input className="input100" type="password" name="password" placeholder="Password"
+                       value={password}
+                       onChange={handleInputChange}/>
                 <span className="focus-input100"/>
             </div>
 
             <div className="row mb-3">
-                <div className="col">
-                    <input className="input-checkbox100" id="ckb1" type="checkbox" name="rememberMe"/>
+                <div className="col"
+                     onClick={toggleCheck}>
+                    <input className="input-checkbox100" id="ckb1" type="checkbox" name="rememberMe"
+                           checked={rememberMe}
+                           readOnly/>
                     <label className="label-checkbox100">
                         Recordarme
                     </label>
@@ -37,7 +73,6 @@ export const LoginPage = () => {
                     Ingresar
                 </button>
             </div>
-
         </form>
     )
 }
