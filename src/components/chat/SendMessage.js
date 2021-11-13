68709185c670cf2a4fa2a7874eb860441b415d8b
unknown <arian.angoma@tecsup.edu.pe>
2021-11-13 11:59:49 -0500

    Formulario de enviar mensaje

--- src/components/chat/SendMessage.js
+++ src/components/chat/SendMessage.js
@@ -1,9 +1,34 @@
+import {useForm} from '../../hooks/useForm';
+
+/* Estado inicial del formulario */
+const initialState = {
+    message: ''
+}
+
 export const SendMessage = () => {
+    /* Hook para el estado del formulario */
+    const [formValue, handleChange, reset] = useForm(initialState);
+    const {message} = formValue;
+
+    /* Función para envíar el formulario */
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        /* Validar si el formulario está vacío */
+        if (!message.length) return null;
+
+        /* Limpiar formulario */
+        reset(initialState);
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="type_msg row">
                 <div className="input_msg_write col-sm-9">
-                    <input type="text" className="write_msg" placeholder="Mensaje..."/>
+                    <input type="text" className="write_msg" placeholder="Mensaje..."
+                           name="message"
+                           value={message}
+                           onChange={handleChange}/>
                 </div>
 
                 <div className="col-sm-3 text-center">
